cc060390c8fd2f7a0104668a6baef77320fd2e27
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-02-24 18:54:26 -0500

    feat: adding problem-9 solution

--- challenges/problem-9.js
+++ challenges/problem-9.js
@@ -1,16 +1,14 @@
 export const checkJump = (heights) =>  {
     const maxValue = Math.max(...heights)
+    const maxValueIndex = heights.indexOf(maxValue)
 
-    const maxValueIndex = heights.findIndex(item => item === maxValue)
+    const maxValueAtStartOrEnd = [0, heights.length - 1].includes(maxValueIndex)
 
     const leftArray = [...heights].slice(0, maxValueIndex)
-    const rightArray = [...heights].slice(maxValueIndex, )
+    const rightArray = [...heights].slice(maxValueIndex,)
 
-    const leftCondition = [...leftArray].sort().reverse() === leftArray
+    const isUp = leftArray.slice(1).every((height, index) => height >= leftArray[index]);
+    const isDown = rightArray.slice(1).every((height, index) => height <= rightArray[index]);
 
-    console.log('fist  ',  [...rightArray].sort().reverse())
-    console.log('const  ', [...rightArray])
-    const rightCondition = [...rightArray].sort().reverse() === rightArray
-    
-    return leftCondition && rightCondition
+    return !maxValueAtStartOrEnd && isUp && isDown
 }
--- tests/problem-9.test.js
+++ tests/problem-9.test.js
@@ -9,5 +9,7 @@ describe('problem-8', () => {
         expect(checkJump([1, 3, 8, 5, 2, 1, 0])).toEqual(true)
         expect(checkJump([1, 18, 8, 5, 2, 1, 0])).toEqual(true)
         expect(checkJump([1, 18, 8, 20, 2, 1, 0])).toEqual(false)
+        expect(checkJump([2, 2, 2, 2])).toEqual(false)
+        expect(checkJump([1, 2, 3])).toEqual(false)
     })
 })
